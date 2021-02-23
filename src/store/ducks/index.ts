07f25989fccdad582132284e6bc3fd8923e0c038
Adriana Saty Hirano Marchioro Bertelli <t731630@MacBook-Pro-de-AdvancedUser.local>
2021-02-23 20:49:18 -0300

    [refactor] deleted exemplo.ducks.ts

--- src/store/ducks/index.ts
+++ src/store/ducks/index.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
-import exampleReducer from './example.duck';
 import todoReducer from './todo.duck';
 
 export default combineReducers({
-  exampleReducer,
   todoReducer,
 });
